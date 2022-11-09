e57faa83a79ec29f276a49c6de8d87053b35255e
MaicolYepess <103972498+MaicolYepess@users.noreply.github.com>
2022-11-09 00:12:25 -0500

    descarga de excel

--- src/app/layout/common/buttons-export/buttons-export.component.ts
+++ src/app/layout/common/buttons-export/buttons-export.component.ts
@@ -13,13 +13,79 @@ export class ButtonsExportComponent implements OnInit {
 
     ngOnInit() {}
 
-    exportar() {
+    exportarViabilidad() {
         this._service.getReport().subscribe(
             (res) => {
               var downloadURL = window.URL.createObjectURL(res);
               var link = document.createElement('a');
               link.href = downloadURL;
               link.download = "Viabilidad";
+              link.click();
+                if (res) {
+                    swal.fire(
+                        'Descarga realizada Exitosamente!',
+                        '',
+                        'success'
+                    );
+                }
+            },
+            (response) => {
+                swal.fire('Error al Registrar la informacion!', '', 'error');
+            }
+        );
+    }
+
+    exportarCdp() {
+        this._service.getReportCdp().subscribe(
+            (res) => {
+              var downloadURL = window.URL.createObjectURL(res);
+              var link = document.createElement('a');
+              link.href = downloadURL;
+              link.download = "SOLICITUD CDP- DAP";
+              link.click();
+                if (res) {
+                    swal.fire(
+                        'Descarga realizada Exitosamente!',
+                        '',
+                        'success'
+                    );
+                }
+            },
+            (response) => {
+                swal.fire('Error al Registrar la informacion!', '', 'error');
+            }
+        );
+    }
+
+    exportarPpa() {
+        this._service.getReportPpa().subscribe(
+            (res) => {
+              var downloadURL = window.URL.createObjectURL(res);
+              var link = document.createElement('a');
+              link.href = downloadURL;
+              link.download = "SOLICITUD PAA";
+              link.click();
+                if (res) {
+                    swal.fire(
+                        'Descarga realizada Exitosamente!',
+                        '',
+                        'success'
+                    );
+                }
+            },
+            (response) => {
+                swal.fire('Error al Registrar la informacion!', '', 'error');
+            }
+        );
+    }
+
+    exportarDap() {
+        this._service.getReportDap().subscribe(
+            (res) => {
+              var downloadURL = window.URL.createObjectURL(res);
+              var link = document.createElement('a');
+              link.href = downloadURL;
+              link.download = "SOLICITUD CONTRATACIÓN DAP";
               link.click();
                 if (res) {
                     swal.fire(
