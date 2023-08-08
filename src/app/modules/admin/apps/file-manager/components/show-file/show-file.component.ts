e3b300585c6c4d926e5e44ce71dd484a359a2aea
jhojanhernandezy24 <jhojanhernandezy24@outlook.es>
2023-08-07 19:47:52 -0500

    ajuste para mostrar archivos

--- src/app/modules/admin/apps/file-manager/components/show-file/show-file.component.ts
+++ src/app/modules/admin/apps/file-manager/components/show-file/show-file.component.ts
@@ -1,72 +1,51 @@
-import { Component, OnInit,Inject, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
 import swal from 'sweetalert2';
-import { interval, Subject,takeUntil } from 'rxjs';
+import { interval, Subject, takeUntil } from 'rxjs';
 import { UploadDataService } from 'app/modules/admin/dashboards/contractual/service/upload-data.service';
 import { ActivatedRoute } from '@angular/router';
 import { FileListManagerService } from '../../services/list-file.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PDFDocument } from 'pdf-lib';
 
 @Component({
-    selector: 'app-register-contractor',
-    templateUrl: './show-file.component.html',
-    styleUrls: ['./show-file.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+  selector: 'app-register-contractor',
+  templateUrl: './show-file.component.html',
+  styleUrls: ['./show-file.component.scss'],
 })
 export class ShowFileComponent implements OnInit {
   shortLink: string = "";
   PathReportString: any;
   getFile: any;
+  fileName: string;
   file: File = null; // Variable to store file
-  contratos: any;    pdfFilePath = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
-  private _unsubscribeAll: Subject<any> = new Subject<any>();
+  contratos: any; 
+  pdfDescomprimido: Uint8Array;
+
   id: any;
   constructor(
-    private _upload: UploadDataService,
-    private _fileManagerService: FileListManagerService,
-    private router: ActivatedRoute,
-    private sanitizer: DomSanitizer
-    ) {}
+    private sanitizer: DomSanitizer,
+    public matDialogRef: MatDialogRef<ShowFileComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data
+  ) { }
 
   ngOnInit(): void {
-    this.id = this.router.snapshot.paramMap.get('id') || 'null';
+    this.id = this._data.id;
+    this.getFile = this._data.fileData;    
+    this.fileName = this._data.fileName;    
 
-    this._fileManagerService.filesContract$
-    .pipe(takeUntil(this._unsubscribeAll))
-    .subscribe((item: any) => {
-        this.id = item.id;
-        // Open the drawer in case it is closed
-        this.getFile = item.filedata;
-        // this.getFile = this.sanitizer.bypassSecurityTrustResourceUrl('data:application/pdf;base64,'+this.getFile);
-    });
-
-    this.PathReportString = 'data:application/pdf;base64,' + (this.sanitizer.bypassSecurityTrustResourceUrl(this.getFile) as any).changingThisBreaksApplicationSecurity;
-      
-    top.document.getElementById('ifrm').setAttribute("src", this.PathReportString);
-    // this.getFile = `data:application/pdf;base64,${this.getFile}`;
-    // const link = document.createElement("a");
-    // link.href = this.getFile;
-    // link.download = `${this.getFile}.pdf`
-    // link.click();
-    console.log(this.PathReportString);
+    // this.getFile = this.getFile.split('data:application/pdf;base64,')
     
+    this.PathReportString = 'data:application/pdf;base64,' + (this.sanitizer.bypassSecurityTrustResourceUrl(this.getFile) as any).changingThisBreaksApplicationSecurity;
+
+    top.document.getElementById('ifrm').setAttribute("src", this.PathReportString);
+
   }
 
-  // downloadPdf(base64String, fileName) {
-  //   this.getFile = `data:application/pdf;base64,${base64String}`;
-  //   const link = document.createElement("a");
-  //   link.href = source;
-  //   link.download = `${fileName}.pdf`
-  //   link.click();
-  // }
-  // onClickDownloadPdf(){
-  //   let base64String = "your-base64-string";
-  //   this.downloadPdf(base64String,"sample");
-  // }
   showPdf() {
     const pdfInBase64 = this.getFile;
-    const newBlob = new Blob([pdfInBase64], {type: 'application/pdf'});
+    const newBlob = new Blob([pdfInBase64], { type: 'application/pdf' });
     // if (window.navigator && window.navigator.msSaveOrOpenBlob) {
     //   window.navigator.msSaveOrOpenBlob(newBlob); // For IE browser
     // }
@@ -77,9 +56,28 @@ export class ShowFileComponent implements OnInit {
     const clickEvent = new MouseEvent('click', {
       'view': window,
       'bubbles': true,
-     'cancelable': false
+      'cancelable': false
     });
     linkElement.dispatchEvent(clickEvent);
+  }
+
+  async descomprimirPDF() {
+    try {
+      // Decodificar el contenido en base64 para obtener el Uint8Array del PDF comprimido
+      const pdfBytesComprimido = Uint8Array.from(atob(this.getFile), c => c.charCodeAt(0));
+
+      // Descomprimir el PDF utilizando pdf-lib
+      const pdfDoc = await PDFDocument.load(pdfBytesComprimido);
+      this.pdfDescomprimido = await pdfDoc.save();
+      // Ahora tienes el PDF descomprimido en this.pdfDescomprimido
+      console.log('PDF descomprimido:', this.pdfDescomprimido);
+    } catch (error) {
+      console.error('Error al descomprimir el PDF:', error);
+    }
+  }
+
+  closeModal(): void {
+    this.matDialogRef.close(true);
   }
 
 
