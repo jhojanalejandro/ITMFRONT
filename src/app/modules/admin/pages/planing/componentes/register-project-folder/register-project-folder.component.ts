b13a155faa18460120c4b7eafa501dec1ea589a5
jhojanhernandezy24 <jhojanhernandezy24@outlook.es>
2023-10-07 03:32:34 -0500

    registro proyecto

--- src/app/modules/admin/pages/planing/componentes/register-project-folder/register-project-folder.component.ts
+++ src/app/modules/admin/pages/planing/componentes/register-project-folder/register-project-folder.component.ts
@@ -8,7 +8,7 @@ import { GlobalConst } from 'app/layout/common/global-constant/global-constant';
 import { UploadDataService } from 'app/modules/admin/dashboards/contractual/service/upload-data.service';
 import { FuseAlertType } from '@fuse/components/alert';
 import { DetailContractFolder, ContractFolder, ContractFolders } from '../../models/planing-model';
-import { GenericService } from 'app/modules/admin/generic/generic.services';
+import { GenericService } from 'app/modules/admin/generic/generic.service';
 import { Subject, takeUntil } from 'rxjs';
 import { RubroType, StatusContract } from 'app/modules/admin/generic/model/generic.model';
 import { CodeStatusContract } from 'app/layout/common/enums/statusContract';
@@ -39,7 +39,6 @@ export class RegisterContractFolderComponent implements OnInit {
   rubroNumber: string;
   editData: boolean = false;
   AddeditData: boolean = false;
-  dataProject: ContractFolders = { companyName: null, projectName: null, statusContract: null, activate: null, contractorsCant: null, valorContrato: null, gastosOperativos: null, valorSubTotal: null, noAdicion: null, fechaInicioAmpliacion: null, fechaDeTerminacionAmpliacion: null, fechaFinalizacion: null, fechaContrato: null, numberProject: null, enableProject: true, project: null, rubro: null, nombreRubro: null, fuenteRubro: null, objectContract: null }
   private _unsubscribeAll: Subject<any> = new Subject<any>();
   permission: boolean = false;
   detailType: string;
@@ -58,24 +57,7 @@ export class RegisterContractFolderComponent implements OnInit {
       this.ref.detectChanges();
       this.ref.markForCheck();
     }, 1000);
-    if (this._data != null) {
-      this.editData = true;
-      this.dataProject.fechaContrato = new Date(this._data.data.fechaContrato)
-      this.dataProject.companyName = this._data.data.companyName;
-      this.dataProject.projectName = this._data.data.projectName;
-      this.dataProject.fechaFinalizacion = new Date(this._data.data.fechaFinalizacion);
-      this.dataProject.numberProject = this._data.data.numberProject;
-      this.dataProject.rubro = this._data.data.rubro;
-      this.dataProject.project = this._data.data.project;
-      this.dataProject.nombreRubro = this._data.data.nombreRubro;
-      this.dataProject.rubroId = this._data.data.rubroId;
-      this.dataProject.fuenteRubro = this._data.data.fuenteRubro;
-      this.dataProject.objectContract = this._data.data.objectContract;
-      this.dataProject.statusContractId = this._data.data.statusContractId;
-      this.detailType = this._data.data.detailType;
 
-
-    }
   }
 
   ngOnInit(): void {
@@ -83,34 +65,63 @@ export class RegisterContractFolderComponent implements OnInit {
     this.permission = this.authService.validateRoll(CodeUser.PLANEACION, null);
     if (!this.permission) {
       Swal.fire('', 'No tienes permisos de modificar Información!', 'warning');
-
     }
 
     this.getStatusContract();
     this.getTypeMinuteContract();
     this.getRubroContract();
     this.formProject = this._formBuilder.group({
-      projectName: new FormControl(this.dataProject.projectName, Validators.required),
-      companyName: new FormControl(this.dataProject.companyName, Validators.required),
-      statusContract: new FormControl(this.dataProject.statusContractId),
-      objectContract: new FormControl(this.dataProject.objectContract),
-      fechaContrato: new FormControl(this.dataProject.fechaContrato, Validators.required),
-      fechaFinalizacion: new FormControl(this.dataProject.fechaFinalizacion, Validators.required),
+      projectName: new FormControl(null, Validators.required),
+      companyName: new FormControl(null, Validators.required),
+      statusContract: new FormControl(null),
+      objectContract: new FormControl(null),
+      fechaContrato: new FormControl(null, Validators.required),
+      fechaFinalizacion: new FormControl(null, Validators.required),
       tipoModificacion: new FormControl(null),
       noAdicion: new FormControl(null),
       fechaInicioAmpliacion: new FormControl(null),
       fechaDeTerminacionAmpliacion: new FormControl(null),
-      numberProject: new FormControl(this.dataProject.numberProject),
-      rubro: new FormControl(this.dataProject.rubroId),
-      nombreRubro: new FormControl(this.dataProject.nombreRubro),
-      fuenteRubro: new FormControl(this.dataProject.fuenteRubro),
-
-      project: new FormControl(this.dataProject.project)
+      numberProject: new FormControl(null),
+      rubro: new FormControl(null),
+      nombreRubro: new FormControl(null),
+      fuenteRubro: new FormControl(null),
+      dutyContract: new FormControl(null),
+      project: new FormControl(null)
     });
-
+    this.fillData();
   }
   ngAfterContentChecked() {
     this.ref.detectChanges();
+  }
+
+  private fillData(){
+    if (this._data != null) {
+      this.editData = true;
+      this.formProject.patchValue({
+        projectName: this._data.data.projectName,
+        companyName: this._data.data.companyName,
+        statusContract: this._data.data.statusContractId,
+        objectContract: this._data.data.objectContract,
+        fechaContrato: new Date(this._data.data.fechaContrato),
+        fechaFinalizacion: new Date(this._data.data.fechaFinalizacion),
+        tipoModificacion: new FormControl(null),
+        noAdicion: new FormControl(null),
+        fechaInicioAmpliacion: new FormControl(null),
+        fechaDeTerminacionAmpliacion: new FormControl(null),
+        numberProject: this._data.data.numberProject,
+        rubro: this._data.data.rubroId,
+        nombreRubro: this._data.data.nombreRubro,
+        fuenteRubro: this._data.data.fuenteRubro,
+        dutyContract: this._data.data.dutyContract,
+        project: this._data.data.project
+      });
+
+      // this.dataProject.rubro = this._data.data.rubro;
+      // this.dataProject.rubroId = this._data.data.rubroId;
+      this.detailType = this._data.data.detailType;
+
+
+    }
   }
   addContractFolder() {
     if (this.formProject.invalid) {
@@ -166,7 +177,8 @@ export class RegisterContractFolderComponent implements OnInit {
         project: this.formProject.value.project,
         rubro: this.formProject.value.rubro,
         nombreRubro: this.formProject.value.nombreRubro,
-        fuenteRubro: this.formProject.value.fuenteRubro
+        fuenteRubro: this.formProject.value.fuenteRubro,
+        dutyContract: this.formProject.value.dutyContract
 
       };
 
@@ -253,7 +265,8 @@ export class RegisterContractFolderComponent implements OnInit {
       project: this.formProject.value.project,
       rubro: this.formProject.value.rubro,
       nombreRubro: this.formProject.value.nombreRubro,
-      fuenteRubro: this.formProject.value.fuenteRubro
+      fuenteRubro: this.formProject.value.fuenteRubro,
+      dutyContract: this.formProject.value.dutyContract
     };
     this._upload.addContractFolder(registerProject)
       .pipe(takeUntil(this._unsubscribeAll))
@@ -304,9 +317,15 @@ export class RegisterContractFolderComponent implements OnInit {
 
   changeRubro(e: any) {
     let dataRubro = this.rubros.find(f => f.id == e.value)
-    this.dataProject.rubro = dataRubro.rubroNumber;
-    this.dataProject.nombreRubro = dataRubro.rubro;
-    this.dataProject.fuenteRubro = dataRubro.rubroOrigin;
+    this.formProject.patchValue({
+      rubro: dataRubro.id,
+      nombreRubro: dataRubro.rubro,
+      fuenteRubro: dataRubro.rubroOrigin,
+    });
+
+    // this.dataProject.rubro = dataRubro.rubroNumber;
+    // this.dataProject.nombreRubro = dataRubro.rubro;
+    // this.dataProject.fuenteRubro = dataRubro.rubroOrigin;
   }
 
   private getRubroContract() {
@@ -331,8 +350,11 @@ export class RegisterContractFolderComponent implements OnInit {
       .pipe(takeUntil(this._unsubscribeAll))
       .subscribe(
         (resp) => {
-          this.detailType = resp.find(f => f.code === DetailTypeCodes.MFC).id
+          this.detailType = resp.find(f => f.code === DetailTypeCodes.CTIL).id
           this.tipoContrato = resp;
+          this.formProject.patchValue({
+            tipoModificacion: this.detailType,
+          });
         }
       );
   }
